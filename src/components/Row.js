694236bb23030857938d9c667b60395569bcd86a
beefstew809 <beefstew809@gmail.com>
2022-11-26 20:25:44 -0800

    Making the guesses go onto the grid

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Row({ guess }) {
+export default function Row({ guess, currentGuess }) {
 
   if (guess) {
     return (
@@ -8,6 +8,20 @@ export default function Row({ guess }) {
             {guess.map((l, i) => (
                 <div key={i} classname={l.color}>{l.key}</div>
             ))}
+        </div>
+    )
+  }
+
+  if (currentGuess) {
+    let letters = currentGuess.split('')
+    return (
+        <div className="row current">
+            {letters.map((letters, i) => (
+                <div key={i} className="filled">{letter} </div>
+            ))}
+            {[...Array(5 - letters.length)].map((_, i) => (
+                <div key={i}></div>
+            ) )}
         </div>
     )
   }
