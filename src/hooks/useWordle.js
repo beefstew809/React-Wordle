5f0c8a7b80b7c30ea2ab0a54e294e16f5d8650aa
beefstew809 <beefstew809@gmail.com>
2022-11-26 18:29:57 -0800

    Making sure this still syncs

---
+++ src/hooks/useWordle.js
@@ -0,0 +1,20 @@
+const useWordle = (solution) => {
+
+   // format a guess into an array of letter objects
+   // e.g. [{key: 'a', color: 'yellow'}]
+   const formatGuess = () => {
+
+   }
+
+   // add a new guess to the guesses state
+   // update the isCorrect state if the guess (whole word) is correct
+   // add one to the turn state
+   const addNewGuess = () => {
+
+   }
+
+
+}
+
+export default useWordle
+
